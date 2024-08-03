ef1dda3533597e3c12cd42ff16c93f82e4ab60aa
mateo <mateosantana2024@gmail.com>
2024-08-03 16:07:01 +0200

    feat: add cleanup on upload script

--- upload.sh
+++ upload.sh
@@ -3,4 +3,5 @@ docker build -t selenium-lambda . --platform=linux/amd64
 docker tag selenium-lambda:latest 374320688826.dkr.ecr.ap-south-1.amazonaws.com/selenium:latest
 docker push 374320688826.dkr.ecr.ap-south-1.amazonaws.com/selenium:latest
 
-
+# Execute ecr cleanup script
+./cleanup.sh
